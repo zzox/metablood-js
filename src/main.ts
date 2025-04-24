0308ee5c2b5294ad67ae512acf160427f50fa00a
zzox <tweedlitweedli@gmail.com>
2025-04-24 00:17:54 -0500

    draw image

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ console.log('we in thissss')
 
 const canvas = document.getElementById('main-canvas') as HTMLCanvasElement
 
+canvas.style.height = '648px'
+canvas.style.width = '480px'
+canvas.style.imageRendering = 'pixelated'
+
 const ctx = canvas!.getContext('2d') as CanvasRenderingContext2D
 
 ctx.fillStyle = 'black'
@@ -14,3 +18,25 @@ ctx.fillText('We In this', 24, 24)
 // notes
 // border type and color for pre-plays
   // needs to be turned on in menu
+
+
+const run = async () => {
+  const image = new Image()
+  image.src = './assets/tiles.png'
+  console.log(image)
+
+  // from: https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images
+  // await Promise.all(
+  //   Array.from(document.images).map(
+  //     (image) =>
+  //       new Promise((resolve) => image.addEventListener("load", resolve)),
+  //   ),
+  // );
+
+  image.addEventListener('load', () => {
+    ctx.drawImage(image, 0, 192, 12, 12, 5, 5, 12, 12)
+  })
+}
+
+
+run()
